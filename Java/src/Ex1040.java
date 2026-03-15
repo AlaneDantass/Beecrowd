48181536f04470ee498c35cc1fe10a7eb0154149
Alane Dantas <156969445+AlaneDantass@users.noreply.github.com>
2026-03-14 21:31:47 -0300

    14/03/2026

--- Java/src/Ex1040.java
+++ Java/src/Ex1040.java
@@ -2,33 +2,33 @@ import java.util.Scanner;
 public class Ex1040{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double A, B, C, D;
-        A = input.nextDouble();
-        B = input.nextDouble();
-        C = input.nextDouble();
-        D = input.nextDouble();
-            
-        double media = (((2*A) + (3*B) + (4*C) + (1*D))*10/10.00);
-        media = Math.round(media ) / 10.0;
+        double N1, N2, N3, N4;
+        N1 = input.nextDouble();
+        N2 = input.nextDouble();
+        N3 = input.nextDouble();
+        N4 = input.nextDouble();
 
-        System.out.printf("Media: %.1f", media);
+        double media = ((2*N1) + (3*N2) + (4*N3) + (1*N4))/10.0;
+        media = ((int)(media * 10)) / 10.0;
+            
+        System.out.printf("Media: %.1f\n", media);
         if( media >= 7.0){
-            System.out.println("\nAluno aprovado.");
+            System.out.println("Aluno aprovado.");
         }else if(media<5.0){
             System.out.println("Aluno reprovado.");
         }else if (media >= 5.0 && media <= 6.9){
-            System.out.println("\nAluno em exame.");
+            System.out.println("Aluno em exame.");
             double exame = input.nextDouble();
-            System.out.printf("Nota do exame: %.1f", exame);
-            media = ((media + exame) / 2.00);
-            media = Math.round(media *10) / 10.0 ;
+            System.out.printf("Nota do exame: %.1f\n", exame);
+            media = ((media + exame) / 2.0);
+            media = ((int)(media * 10)) / 10.0;
             if(media >= 5.0){
-                System.out.println("\nAluno aprovado.");
+                System.out.println("Aluno aprovado.");
             }else if (media <= 4.9){
-                System.out.println("\nAluno reprovado.");
+                System.out.println("Aluno reprovado.");
             }
         }
-        System.out.printf("Media final: %.1f", media);
+        System.out.printf("Media final: %.1f\n", media);
     input.close();
     }
 }
